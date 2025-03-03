2a59e4018683ed9e09e4798827fdbe9c78d0885b
Falsidical <franport@outlook.com>
2025-03-03 07:30:02 +0000

    TODO revision

--- game.js
+++ game.js
@@ -5,6 +5,13 @@ const cancelBtn = document.querySelector('.cancel');
 const dialog = document.querySelector('dialog');
 const form = document.querySelector('form');
 
+const name1 = document.querySelector('.name1');
+const name2 = document.querySelector('.name2');
+const score1 = document.querySelector('.p1score');
+const score2 = document.querySelector('.p2score');
+
+const header = document.querySelector('h1');
+
 function Player(name, marker) {
   this.name = name;
   this.marker = marker;
@@ -12,23 +19,25 @@ function Player(name, marker) {
 }
 
 const gameController = (function () {
-  const gameBoard = [];
-  let p1, p2;
-  let turn = 0;
+  let gameBoard = [];
+  let p1, p2, turn;
+  let gameActive = false;
 
   const startGame = function (player1, player2) {
     [p1, p2] = [player1, player2];
+    turn = 0;
+    gameBoard = [];
+    displayController.draw(gameBoard);
+    gameActive = true;
+    displayController.updatePlayers(p1, p2);
   };
 
   const playerTurn = function (position) {
-    if (turn % 2 === 0) {
-      gameBoard[position] = p1.marker;
-    } else {
-      gameBoard[position] = p2.marker;
-    }
+    if (!gameActive || gameBoard[position] !== undefined) return;
+    gameBoard[position] = turn % 2 === 0 ? p1.marker : p2.marker;
     turn++;
-    displayController.draw(gameBoard);
     checkGameOver();
+    displayController.draw(gameBoard);
   };
 
   const winningPos = [
@@ -46,10 +55,22 @@ const gameController = (function () {
     [p1, p2].forEach((player) => {
       winningPos.forEach((pos) => {
         if (gameBoard[pos[0]] === player.marker && gameBoard[pos[1]] === player.marker && gameBoard[pos[2]] === player.marker) {
-          console.log(`gano el jugador ${player.name}`);
+          displayController.updatePlayers(p1, p2);
+          gameOver(player);
         }
       });
     });
+    if (turn === 9) {
+      displayController.updateHeader('Draw! New round');
+      startGame(p1, p2);
+    }
+  };
+
+  const gameOver = function (player) {
+    gameActive = false;
+    displayController.updateHeader(`${player.name} won in ${turn} turns.`);
+    player.score++;
+    startGame(p1, p2);
   };
 
   return { playerTurn, startGame };
@@ -59,6 +80,18 @@ const displayController = (function () {
   const clear = function () {
     buttons.forEach((btn) => (btn.textContent = ''));
   };
+
+  const updatePlayers = function (player1, player2) {
+    name1.textContent = player1.name;
+    score1.textContent = player1.score;
+    name2.textContent = player2.name;
+    score2.textContent = player2.score;
+  };
+
+  const updateHeader = function (text) {
+    header.textContent = text;
+  };
+
   const draw = function (boardArray) {
     clear();
     for (let i = 0; i < 9; i++) {
@@ -66,7 +99,7 @@ const displayController = (function () {
     }
   };
 
-  return { draw };
+  return { draw, updatePlayers, updateHeader };
 })();
 
 for (let i = 0; i < 9; i++) {
